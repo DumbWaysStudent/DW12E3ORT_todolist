30596f81775d5e9020c735fb60b2f994ce56a9d1
Sony Alamsyah M <sonyalamsyahmegadiana92@gmail.com>
2019-10-07 19:27:29 +0700

    add 3.delete_todo

---
+++ App.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import { Container, Content, List, ListItem, Button, Text, Input, Item, Body, Icon, Right } from 'native-base';
+
+class Todos extends Component{
+  constructor() {
+        super();
+        this.state = {
+          inputValue : '',
+          dataKegiatan: [
+            'work',
+            'swim',
+            'study',
+            'sleep',
+            'run'
+          ]
+        } 
+      }
+
+    setInputValue = (input) => {
+      this.setState({inputValue: input})
+    }
+
+    onClickAdd = () => {
+      const newDataKegiatan = this.state.dataKegiatan.concat(this.state.inputValue)
+      this.setState({dataKegiatan: newDataKegiatan, inputValue: ''})
+    }
+
+    onDelete = (index) => {
+      const newDataKegiatan = this.state.dataKegiatan.filter((value, idx, arr) => {
+          return (index != idx)
+      });
+      this.setState({dataKegiatan: newDataKegiatan})
+    }
+
+    renderList = () => {
+      return this.state.dataKegiatan.map((key, index) => {
+        return (
+          <ListItem icon key={index}>
+            <Body>
+              <Text>{key}</Text>
+            </Body>
+            <Right>
+             <Button danger onPress = {() => this.onDelete(index)}>
+               <Icon active name="trash" />
+             </Button>
+            </Right>
+          </ListItem>
+        );
+      });
+    }
+
+    render(){
+      return(
+        <Container>
+          <Content>
+            <Body style={styles.TextBox1}>
+              <Item regular style={styles.childOfTexBox1}>
+                <Input
+                  placeholder='New to do'
+                  onChangeText={e => this.setInputValue(e)}
+                  value={this.state.inputValue}
+                />
+              </Item>
+              <Button bordered style={styles.childOfTexBox2} onPress={this.onClickAdd}>
+                <Text>Add</Text>
+              </Button>
+              </Body>
+            <List>
+              {this.renderList()}
+            </List>
+          </Content>
+        </Container>
+      );
+    }
+}
+
+const styles = {
+  TextBox1:{
+    flexDirection: 'row'
+  },
+  childOfTexBox1:{
+    flex: 8
+  },
+  childOfTexBox2:{
+    flex: 2
+  }
+}
+
+export default Todos
